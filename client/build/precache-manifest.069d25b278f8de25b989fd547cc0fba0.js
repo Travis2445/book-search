da47614305e22456cca31e99a4e803bf3d2a47ee
Travis2445 <90297320+Travis2445@users.noreply.github.com>
2022-04-01 16:40:36 -0500

    build

---
+++ client/build/precache-manifest.069d25b278f8de25b989fd547cc0fba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19290cdc4a0a8719e244905bf2ae0bc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4adaad1f8b8f81f38d6a",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "e16f2d704744798799bc",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "4adaad1f8b8f81f38d6a",
+    "url": "/static/js/2.c6eab958.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.c6eab958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e16f2d704744798799bc",
+    "url": "/static/js/main.a5e25ee1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
